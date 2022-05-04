5ff916e17d56735a72c3e665d465128df2a76839
crisptech <crisptech@hotmail.com>
2022-05-04 18:02:23 +0100

    fix: remove npm package lock file, to depend on yarn

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,12 @@
 const API_URL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
-module.exports = API_URL;
+const APOLLO_KEY = "y9fb83YKKFYa2Ll1KPMUMg";
+const APOLLO_GRAPH_REF = "current";
+const APOLLO_SCHEMA_REPORTING = true;
+
+module.exports = {
+  API_URL,
+  APOLLO_KEY,
+  APOLLO_GRAPH_REF,
+  APOLLO_SCHEMA_REPORTING,
+};
