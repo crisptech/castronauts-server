f45f12874bf7fd8cdf8a4897c540dd37f9e1faa1
crisptech <crisptech@hotmail.com>
2022-05-05 09:42:11 +0100

    fix: remove unnecessary environmental variables from the constants file

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,3 @@
 const API_URL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
-const APOLLO_KEY = "y9fb83YKKFYa2Ll1KPMUMg";
-const APOLLO_GRAPH_REF = "current";
-const APOLLO_SCHEMA_REPORTING = true;
-
-module.exports = {
-  API_URL,
-  APOLLO_KEY,
-  APOLLO_GRAPH_REF,
-  APOLLO_SCHEMA_REPORTING,
-};
+module.exports = API_URL;
